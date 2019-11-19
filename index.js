ac75d397f7ae89f90eb7ca3b2406b4c1f147dfa6
Andrew614 <azhang213@gmail.com>
2019-11-19 02:02:20 -0500

    add apis to get all people and one person

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
 const express = require('express')
 const app = express()
 
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }
+]
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>')
+})
+
+app.get('/info', (req, res) => {
+    res.send(`<div>
+        <p>Phonebook has info for ${persons.length} people<p> 
+        <p>${new Date()}</p>
+    </div>`)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.find(p => p.id == id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
 })
 
 const PORT = 3001
