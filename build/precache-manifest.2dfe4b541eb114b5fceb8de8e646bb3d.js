a757b0568778f7af8e419b1c476ca5d4714c6535
Andrew614 <azhang213@gmail.com>
2019-11-25 20:41:12 -0500

    add static middleware and production build

---
+++ build/precache-manifest.2dfe4b541eb114b5fceb8de8e646bb3d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d673b73ab7197f155dda8f28f2053d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c37ff1d967f77cd5dd2",
+    "url": "/static/js/2.608e7dc9.chunk.js"
+  },
+  {
+    "revision": "2a31e802a948d075e3eb",
+    "url": "/static/js/main.edb150f1.chunk.js"
+  },
+  {
+    "revision": "958ffb7c649ae071635d",
+    "url": "/static/js/runtime-main.4f8f3cdb.js"
+  }
+]);
